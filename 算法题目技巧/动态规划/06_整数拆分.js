b8077fc0f8d1c22488fb977d89b2129a852fd13c
2019083310 <2330053403@qq.com>
2022-04-12 20:36:09 +0800

    write tools

---
+++ 算法题目技巧/动态规划/06_整数拆分.js
@@ -0,0 +1,2 @@
+// https://leetcode-cn.com/problems/integer-break/
+
---
+++ 算法题目技巧/树/09_二叉树的最近公共祖先.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  const postOrder = (root, p, q) => {
+    if (!root || p === root || q === root) {
+      return root
+    }
+    let left = postOrder(root.left, p, q)
+    let right = postOrder(root.right, p, q)
+    if (left && right) {
+      return root
+    }
+    if (!left) {
+      return right
+    }
+    return left
+  }
+  return postOrder(root, p, q)
+};
