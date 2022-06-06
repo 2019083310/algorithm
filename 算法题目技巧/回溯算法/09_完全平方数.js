86183d07b0ed4eb5bcb2afa077c1bb29dcb5d653
2019083310 <2330053403@qq.com>
2022-06-06 15:05:41 +0800

    day01 array+string+map

--- 算法题目技巧/回溯算法/09_完全平方数.js
+++ 算法题目技巧/回溯算法/09_完全平方数.js
@@ -4,17 +4,18 @@
  */
 var numSquares = function (n) {
   // BFS思想
-  let queue = [n];
-  let visited = {};
-  let level = 0;
+  let queue = [n]
+  let level = 0
+  let visited = {}
+
   while (queue.length > 0) {
-    level++;
+    level++
     for (let i = 0, len = queue.length; i < len; i++) {
-      let cur = queue.shift()
+      const cur = queue.shift()
       for (let j = 1; j * j <= cur; j++) {
-        const tmp = cur - j * j;
-        if (tmp === 0) return level;
-        if (!visited[tmp]) { //避免相同值的节点进行重复操作
+        let tmp = cur - j * j
+        if (tmp === 0) return level
+        if (!visited[tmp]) {
           visited[tmp] = true
           queue.push(tmp)
         }
