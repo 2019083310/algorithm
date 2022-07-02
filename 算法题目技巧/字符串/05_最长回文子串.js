ed5c5c3d2a4821dd5bd5f3e030b43e16ddff89d2
2019083310 <2330053403@qq.com>
2022-07-02 15:38:45 +0800

    lc热题一百

--- 算法题目技巧/字符串/05_最长回文子串.js
+++ 算法题目技巧/字符串/05_最长回文子串.js
@@ -2,29 +2,64 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {
-  // 暴力解法
-  let res = 1
-  let maxLen = s[0]
+
+// ?1.暴力解法
+function longestPalindrome(s) {
+  let res = ''
   for (let i = 0; i < s.length; i++) {
+    let str = s[i]
+    if (isPalindrome) {
+      res = res.length < str.length ? str : res
+    }
     for (let j = i + 1; j < s.length; j++) {
-      const str = s.slice(i, j + 1)
-      if (isPalindrome(str) && str.length > res) {
-        res = str.length
-        maxLen = str
+      str += s[j]
+      if (isPalindrome(str)) {
+        res = res.length < str.length ? str : res
       }
     }
   }
-  return maxLen
-};
 
-function isPalindrome(s) {
-  let end = s.length - 1
+  return res
+}
+
+// ?2.找到字符串s的所有子集，判断,并不正确
+function longestPalindrome(s) {
+  const set = new Set()
+  const path = []
+  let res = ''
+
+  backTracking(0)
+
+  function backTracking(startIndex) {
+    set.add(path.join(''))
+
+    for (let i = startIndex; i < s.length; i++) {
+      path.push(s[i])
+      backTracking(i + 1)
+      path.pop()
+    }
+  }
+
+  for (const str of set) {
+    if (isPalindrome(str)) {
+      res = res.length > str.length ? res : str
+    }
+  }
+  console.log(res)
+}
+
+longestPalindrome('babad')
+
+// ?判断是否是回文字符串
+function isPalindrome(str) {
+  let end = str.length - 1
   for (let i = 0; i <= end; i++) {
-    if (s[i] !== s[end]) {
+    if (str[i] !== str[end]) {
       return false
     }
+
     end--
   }
+
   return true
 }
